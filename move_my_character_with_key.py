ecf481836412e834f6b85cf52e002e1a16c187b7
TUJungMin <paulkjm92@gmail.com>
2023-09-25 22:29:04 +0900

    필요없는 사진들 삭제

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -8,13 +8,14 @@ character = load_image('character.png')
 
 
 def handle_events():
-    global running, move_x, direction,move_y
+    global running, move_x, direction, move_y, Move_nothing
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
+            Move_nothing = False
             if event.key == SDLK_RIGHT:
                 move_x += 1
                 direction = 0
@@ -30,6 +31,7 @@ def handle_events():
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
+            Move_nothing = True
             if event.key == SDLK_RIGHT:
                 move_x -= 1
                 direction = 0
@@ -42,6 +44,9 @@ def handle_events():
             elif event.key == SDLK_DOWN:
                 move_y += 1
                 direction = 1
+        else:
+            Move_nothing = True
+
 
 running = True
 x = TUK_WIDTH // 2
@@ -50,15 +55,25 @@ frame = 0
 direction = 0;
 move_x = 0
 move_y = 0
-
+Move_nothing = False
+move_shape = 100
 # fill here
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame*64,direction*64,64,64,x,y)
+    character.clip_draw(frame*64,direction*64,64,64,x,y,100,move_shape)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 9
+    if Move_nothing:
+        # Move_nothing 활성화 시 move_shape 조절
+        move_shape = (move_shape -1)%20+80
+    else:
+        move_shape = 100
+
+
+
+
     if(x>TUK_WIDTH):
         x = TUK_WIDTH
     elif(x<0):
